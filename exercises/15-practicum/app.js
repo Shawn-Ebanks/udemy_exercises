fc767898c6b8a47042cf507041d778c17de17742
Shawn Ebanks <shawnebanks@rocketmail.com>
2021-03-24 19:43:29 -0500

    practice ecerices

---
+++ exercises/15-practicum/app.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+var $stepperInput = $('.stepperInput input');
+
+function incrementStepperInput(amount) {
+  $stepperInput.val((parseInt($stepperInput.val()) || 0) + amount);
+}
+
+var stepperInputDecrement = $('.stepperInput button')[0];
+$(stepperInputDecrement).click(() => {
+  incrementStepperInput(-1);
+});
+
+var stepperInputIncrement = $('.stepperInput button')[1];
+$(stepperInputIncrement).click(() => {
+  incrementStepperInput(1);
+});
